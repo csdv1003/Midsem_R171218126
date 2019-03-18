bd67f86880d823bbf99238066cb7b0a4f905502f
nit29sin <nitish29singla@gmail.com>
2019-03-18 12:33:55 +0530

    firstcommit

---
+++ c++program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespacestd;
+class calculator
+{
+	int add (int x + int y)
+	{
+		return (int x +int y)
+	}
+};
+int main()
+{
+	calculator c;
+	c.add (15,20)
+	return 0;
+}	
