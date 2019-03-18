ad42d91c64bdd99ca5d4a3ebe8218e2e7a5dd1f8
nit29sin <nitish29singla@gmail.com>
2019-03-18 12:43:09 +0530

    thirdcommit

--- c++program.cpp
+++ c++program.cpp
@@ -4,12 +4,17 @@ class calculator
 {
 	int add (int x + int y)
 	{
-		return (int x +int y)
+		return (int x +int y);
 	}
 	int sub (int x - int y)
 	{
-		return (int x - int y)
+		return (int x - int y);
 	}
+	int mul (int x * int y)
+	{
+		return (int x * int y);
+	}
+
 
 };
 int main()
@@ -17,6 +22,7 @@ int main()
 	calculator c;
 	c.add (15,20);
 	c.sub (30,20);
+	c.mul (5,10);
 	return 0;
 }
 
