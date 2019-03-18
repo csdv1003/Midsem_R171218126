70b129806f6dea7a9f3ffa815ae8dc0041da918f
nit29sin <nitish29singla@gmail.com>
2019-03-18 12:39:59 +0530

    secondcommit

--- c++program.cpp
+++ c++program.cpp
@@ -6,10 +6,17 @@ class calculator
 	{
 		return (int x +int y)
 	}
+	int sub (int x - int y)
+	{
+		return (int x - int y)
+	}
+
 };
 int main()
 {
 	calculator c;
-	c.add (15,20)
+	c.add (15,20);
+	c.sub (30,20);
 	return 0;
-}	
+}
+
